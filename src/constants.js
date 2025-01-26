f3adbeb0494c95453a68a075b4e3dcfdd4a5c956
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-26 08:27:50 +0545

    update and refactor constants file

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,15 @@
-export const DB_NAME = "nodetube";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const ENV_KEYS = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  PORT: process.env.PORT || 3000,
+  DB_NAME: process.env.DB_NAME,
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
+};
+
+export const APP_CONSTANTS = {
+  JSON_LIMIT: "16kb",
+  URL_ENCODED_LIMIT: "16kb",
+};
