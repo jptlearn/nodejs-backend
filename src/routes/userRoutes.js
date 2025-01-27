63393a710bf899ebfbc2e8c2f8acba145e2750af
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-27 15:40:27 +0545

    add multer middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,21 @@
 import express from "express";
 import { registerUser } from "../controllers/userController.js";
+import { upload } from "../middlewares/multerMiddleware.js";
 
 const router = express.Router();
 
-router.route("/register").get(registerUser);
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
