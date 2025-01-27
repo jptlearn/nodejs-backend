b466ed4c7e969317d17363075f896277992d7fc9
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-27 15:07:43 +0545

    refactor: change the code to promise based

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,7 @@
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (error) {
-    res.status(error.code || 500).json({
-      success: false,
-      message: error.message,
-    });
-  }
+const asyncHandler = (fn) => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
 };
 
-export default asyncHandler;
+export { asyncHandler };
