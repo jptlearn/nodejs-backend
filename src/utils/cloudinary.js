68e24934bc867c63f6b5916666620a0e81ea161c
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-27 13:25:09 +0545

    feat(utils): add cloudinary file upload utility

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import "dotenv/config";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click above to copy your API secret
+});
+
+const uploadOnCloudinary = async (localFilePath, folder) => {
+  try {
+    if (!localFilePath) {
+      throw new Error("Please provide a valid file path");
+    }
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    // file has been uploaded successfully
+    console.log("file uploaded successfully");
+    console.log(response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation failed
+    return null;
+  }
+};
+
+export default uploadOnCloudinary;
