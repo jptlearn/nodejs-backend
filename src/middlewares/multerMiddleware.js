97efd7733a5129ecb500de2873f992e4341d7987
skcvlearn404 <skcvlearn404@gmail.com>
2025-01-27 15:39:46 +0545

    refactor filename logic

--- src/middlewares/multerMiddleware.js
+++ src/middlewares/multerMiddleware.js
@@ -5,11 +5,12 @@ const storage = multer.diskStorage({
     cb(null, "./public/temp");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, file.fieldname + "-" + uniqueSuffix);
+    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    // cb(null, file.fieldname + "-" + uniqueSuffix);
+    cb(null, file.originalname);
   },
 });
 
 const upload = multer({ storage: storage });
 
-export default upload;
+export { upload };
